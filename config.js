53032107e893406a8910af8c06e040d7b2483923
PengJu Zhao <564357027@qq.com>
2025-07-16 22:20:13 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // config.js - 配置模块
 export const GITHUB_CONFIG = {
-    username: 'FMengz', // 替换为您的GitHub用户名
-    repo: 'questions.github.io',         // 替换为您的仓库名称
-    token: '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',   // 替换为您的个人访问令牌
-    branch: 'main',                 // 默认分支
+    username: 'FMengz', 
+    repo: 'questions.github.io', 
+    token: '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', 
+    branch: 'main',  
     filePath: 'data/selected_topics.json' // 数据文件路径
 };
