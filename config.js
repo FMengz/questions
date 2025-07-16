ca53067cba3b4a66b82c26091e81428522ee235f
PengJu Zhao <564357027@qq.com>
2025-07-16 22:23:48 +0800

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-// config.js - 配置模块
-export const GITHUB_CONFIG = {
-    username: 'FMengz', 
-    repo: 'questions.github.io', 
-    token: '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', 
-    branch: 'main',  
-    filePath: 'data/selected_topics.json' // 数据文件路径
-};
